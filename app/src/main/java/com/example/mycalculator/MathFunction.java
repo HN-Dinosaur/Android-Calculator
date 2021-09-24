adca8d17cdd43fb0c1ef5dc994804e8c3d476568
HN-Dinosaur <464050079@qq.com>
2021-09-24 17:10:18 +0800

    basic MathFunction

--- app/src/main/java/com/example/mycalculator/MathFunction.java
+++ app/src/main/java/com/example/mycalculator/MathFunction.java
@@ -1,5 +1,54 @@
 package com.example.mycalculator;
 
 public class MathFunction {
-
+    public static int operatorNum = 0;
+    private static double calculatorNum = 0.0;
+    //按数字按钮
+    public static String addNum(String displayText,String addNum){
+        if(displayText.equals("0")){
+            return "";
+        }
+        if(Calculator.isLonger(displayText)){
+            return displayText;
+        }
+        return displayText + addNum;
+    }
+    public static double returnSin(Double num){
+        return Math.sin(num);
+    }
+    public static double returnCos(Double num){
+        return Math.cos(num);
+    }
+    public static double returnTan(Double num){
+        return Math.tan(num);
+    }
+    public static double processResult(String display){
+        processDisplayString(display);
+        if(operatorNum == 1){
+            return returnSin(calculatorNum);
+        }else if(operatorNum == 2){
+            return returnCos(calculatorNum);
+        }else{
+            return returnTan(calculatorNum);
+        }
+    }
+    public static void setOperatorNum(int num){
+        operatorNum = num;
+    }
+    //得到纯数字
+    public static int getOperatorNum(){
+        return operatorNum;
+    }
+    //处理当前显示字符串  只剩数字
+    public static void processDisplayString(String display){
+        String deleteStr = "";
+        if(operatorNum == 1){
+            deleteStr = display.replace("sin","");
+        }else if(operatorNum == 2){
+            deleteStr = display.replace("cos","");
+        }else{
+            deleteStr = display.replace("tan","");
+        }
+        calculatorNum = Double.parseDouble(deleteStr);
+    }
 }
--- app/src/main/java/com/example/mycalculator/MathFunctionActivity.java
+++ app/src/main/java/com/example/mycalculator/MathFunctionActivity.java
@@ -7,10 +7,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MathFunctionActivity extends AppCompatActivity {
 
+    String text = null;
     Intent intent = null;
+    TextView displayText = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +23,7 @@ public class MathFunctionActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.math_menu, menu);
+        displayText = findViewById(R.id.displayText);
         return true;
     }
 
@@ -27,17 +32,14 @@ public class MathFunctionActivity extends AppCompatActivity {
 
         switch (item.getItemId()) {
             case R.id.main_menu:
-//                Toast.makeText(this, "conversion_of_number_systems", Toast.LENGTH_SHORT).show();
                 intent = new Intent(MathFunctionActivity.this,MainActivity.class);
                 startActivity(intent);
                 break;
             case R.id.conversion_of_number_systems:
-//                Toast.makeText(this, "math_function", Toast.LENGTH_SHORT).show();
                 intent = new Intent(MathFunctionActivity.this,ConversionOfNumberSystemsActivity.class);
                 startActivity(intent);
                 break;
             case R.id.unit_conversion:
-//                Toast.makeText(this, "unit_conversion", Toast.LENGTH_SHORT).show();
                 intent = new Intent(MathFunctionActivity.this,UnitConversionActivity.class);
                 startActivity(intent);
                 break;
@@ -45,5 +47,41 @@ public class MathFunctionActivity extends AppCompatActivity {
         }
 
         return true;
+    }
+    public void handleClickRight(View view){
+        String tag = String.valueOf(view.getTag());
+        String display = String.valueOf(displayText.getText());
+        //AC
+        if(tag.equals("1")){
+            text = "0";
+            //等于
+        }else{
+            text = String.valueOf(MathFunction.processResult(display));
+        }
+        displayText.setText(text);
+    }
+    public void handleClickTop(View view){
+        String tag = String.valueOf(view.getTag());
+        //sin
+        if(tag.equals("1")){
+            MathFunction.setOperatorNum(1);
+            text = "sin";
+            //cos
+        }else if(tag.equals("2")){
+            MathFunction.setOperatorNum(2);
+            text = "cos";
+            //tan
+        }else{
+            MathFunction.setOperatorNum(3);
+            text = "tan";
+        }
+        displayText.setText(text);
+    }
+    public void handleClickNum(View view){
+        //点击数字按钮
+        String display = String.valueOf(displayText.getText());
+        String tag = String.valueOf(view.getTag());
+        text = MathFunction.addNum(display,tag);
+        displayText.setText(text);
     }
 }
